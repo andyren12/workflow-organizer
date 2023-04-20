cbd1df0c809dd33cc5486eda27f2328baeb77a1b
Andy Ren <andyren33@gmail.com>
2023-04-19 21:07:22 -0700

    Create Projects tab to upload media files connected to Supabase storage

--- client/src/Projects.js
+++ client/src/Projects.js
@@ -1,26 +1,88 @@
-import { useState } from 'react';
+import { GoogleLogin } from './GoogleLogin'
+import { useState, useEffect } from 'react';
+import { Form, Button, Row, Col, Card } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import ReactPlayer from 'react-player'
+import { v4 as uuidv4 } from 'uuid'
 
 export default function Projects() {
-    const [image, setImage] = useState(null);
-    const [fileName, setFileName] = useState("No selected file");
+    const { supabase, user } = GoogleLogin();
+    const [images, setImages] = useState([]);
+    const [playing, setPlaying] = useState(false);
+
+    const CDNURL = "https://xpdqyzszorntzkrorvdx.supabase.co/storage/v1/object/public/Project%20files/";
+
+    async function getImages() {
+        const { data, error } = await supabase
+            .storage
+            .from('Project files')
+            .list(user?.id + "/", {
+                limit: 100,
+                offset: 0,
+                sortBy: { column: "name", order: "asc" }
+            });
+        
+            if(data !== null) {
+                setImages(data);
+            } else {
+                console.log(error);
+            }
+    }
+
+    async function uploadImage(e) {
+        let file = e.target.files[0];
+        const { data, error } = await supabase
+            .storage
+            .from('Project files')
+            .upload(user.id + "/" + uuidv4(), file)
+
+        if(data) {
+            getImages();
+        } else {
+            console.log(error);
+        }
+    }
+
+    async function deleteImage(imageName) {
+        const { error } = await supabase
+        .storage
+        .from('Project files')
+        .remove([user.id + "/" + imageName])
+
+        if(error) {
+            alert(error);
+        } else {
+            getImages();
+        }
+    }
+
+    useEffect(() => {
+        if(user) {
+            getImages();
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [user])
 
     return (
-        <div style={{ display: "flex", flexDirection: "column", alignItems: "center", marginTop: "300px" }}>
-            <input type="file" onChange={({ target: {files}}) => {
-                files[0] && setFileName(files[0].name)
-                if (files) {
-                    setImage(URL.createObjectURL(files[0]))
-                }
-                console.log(files);
-            }}/>
-            {image ? 
-            <img src={image} alt={fileName} width={80} height={80}/>
-            :
-            <div> no image yet </div>}
-            <button onClick={() => {
-                setFileName("No selected file");
-                setImage(null);
-            }}>Delete</button>
+        <div style={{margin: "300px"}}>
+            <Form.Group className="mb-3" style={{width: "220px"}}>
+                <Form.Control type="file" accept="image/png, image/jpg, video/mp4" onChange={(e) => uploadImage(e)}/>
+            </Form.Group>
+            <Row xs={1} md={3} className="g-4">
+                {images.map((image) => {
+                    return (
+                        <Col key={CDNURL + user.id + "/" + image.name}>
+                            <Card>
+                                <ReactPlayer url={CDNURL + user.id + "/" + image.name} playing={playing} />
+                                <Button onClick={() => { setPlaying(true) }}>Play</Button>
+                                <Card.Body>
+                                    <Button variant="danger" onClick={() => deleteImage(image.name)}>Delete</Button>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    )
+                })}
+            </Row>
         </div>
     )
 }
