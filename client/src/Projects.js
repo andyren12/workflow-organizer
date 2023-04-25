87a8a5098ee46687ce57ba33ea9b07e01d4c808a
Andy Ren <andyren33@gmail.com>
2023-04-25 12:04:21 -0700

    Add video control for video files. Change delete button to an 'x'

--- client/src/Projects.js
+++ client/src/Projects.js
@@ -2,16 +2,15 @@ import { GoogleLogin } from './GoogleLogin'
 import { useState, useEffect } from 'react'
 import ReactPlayer from 'react-player'
 import { v4 as uuidv4 } from 'uuid'
-import styles from "./Projects.module.scss"
+import styles from './Projects.module.scss'
 
 export default function Projects() {
     const { supabase, user } = GoogleLogin();
-    const [images, setImages] = useState([]);
-    const [playing, setPlaying] = useState(false);
+    const [videos, setVideos] = useState([]);
 
     const CDNURL = "https://xpdqyzszorntzkrorvdx.supabase.co/storage/v1/object/public/Project%20files/";
 
-    async function getImages() {
+    async function getVideos() {
         const { data, error } = await supabase
             .storage
             .from('Project files')
@@ -22,13 +21,13 @@ export default function Projects() {
             });
         
         if(data !== null) {
-            setImages(data);
+            setVideos(data);
         } else {
             console.log(error);
         }
     }
 
-    async function uploadImage(e) {
+    async function uploadVideos(e) {
         let file = e.target.files[0];
         const { data, error } = await supabase
             .storage
@@ -36,28 +35,28 @@ export default function Projects() {
             .upload(user.id + "/" + uuidv4(), file)
 
         if(data) {
-            getImages();
+            getVideos();
         } else {
             console.log(error);
         }
     }
 
-    async function deleteImage(imageName) {
+    async function deleteVideo(videoName) {
         const { error } = await supabase
         .storage
         .from('Project files')
-        .remove([user.id + "/" + imageName])
+        .remove([user.id + "/" + videoName])
 
         if(error) {
             alert(error);
         } else {
-            getImages();
+            getVideos();
         }
     }
 
     useEffect(() => {
         if(user) {
-            getImages();
+            getVideos();
         }
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [user])
@@ -65,24 +64,23 @@ export default function Projects() {
     return (
         <div className={styles.main}>
             <div className={styles.head}>
-                <input id="file-upload" type="file" accept="video/mp4" onChange={(e) => uploadImage(e)}/>
+                <input id="file-upload" type="file" accept="video/mp4" onChange={(e) => uploadVideos(e)}/>
                 <label htmlFor="file-upload">
                     <div className={styles.uploadButton}>
-                        Upload
+                        Upload File
                     </div>
                 </label>
                 <h2 className={styles.headText}>Your Media <hr /></h2>
             </div>
-            <div className={styles.images}>
-                    {images.map((image) => {
-                        return (
-                            <div key={image.name} className={styles.imageCard}>
-                                <ReactPlayer url={CDNURL + user.id + "/" + image.name} playing={playing} width='360px' height='270px'/>
-                                <button onClick={() => { setPlaying(true) }}>Play</button>
-                                <button onClick={() => deleteImage(image.name)}>Delete</button>
-                            </div>
-                        )
-                    })}
+            <div className={styles.videos}>
+                {videos.map((video, index) => {
+                    return (
+                        <div className={styles.vidCard} key={video.name}>
+                                <ReactPlayer url={CDNURL + user.id + "/" + video.name} controls='true' width='360px' height='270px'/>
+                                <button id={styles.delButton} onClick={() => deleteVideo(video.name)}></button>
+                        </div>
+                    )
+                })}
             </div>
         </div>
     )
