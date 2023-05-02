0b94f2f8b10eebb77798e8a19aea86393a1b74c8
Andy Ren <andyren33@gmail.com>
2023-05-01 19:37:57 -0700

    Integrate calendar with task list in add to calendar feature

--- client/src/Calendar.js
+++ client/src/Calendar.js
@@ -2,16 +2,17 @@ import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider'
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 import dayjs from 'dayjs';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { GoogleLogin } from './GoogleLogin';
+import styles from './Calendar.module.scss'
 
 export default function Calendar() {
     const [start, setStart] = useState(new dayjs(Date()));
     const [end, setEnd] = useState(new dayjs(Date()));
     const [eventName, setEventName] = useState("")
     const [eventDescription, setEventDescription] = useState("")
-    const [upcomingEvents, setUpcomingEvents] = useState([])
-    const [upcomingEventNames, setUpcomingEventNames] = useState([])
+    // const [upcomingEvents, setUpcomingEvents] = useState([])
+    // const [upcomingEventNames, setUpcomingEventNames] = useState([])
 
     const { session } = GoogleLogin();
   
@@ -37,51 +38,47 @@ export default function Calendar() {
       })
     }
 
-    async function getCalendarEvent() {
-      await fetch("https://www.googleapis.com/calendar/v3/calendars/primary/events?" + new URLSearchParams({
-        singleEvents: true,
-        orderBy: 'startTime',
-        maxResults: 10,
-        timeMin: '2023-04-10T23:59:59-08:00'
-      }), {
-        method: "GET",
-        headers: {
-          'Authorization': 'Bearer ' + session.provider_token
-        },
-      }).then((res) => {
-        return res.json()
-      }).then((response) => {
-        setUpcomingEvents(response.items)
-        setUpcomingEventNames(response.items.map(({ summary }) => summary))
-      })
-    }
-
-    useEffect(() => {
-      console.log(upcomingEvents)
-    }, [upcomingEvents])
+    // async function getCalendarEvent() {
+    //   await fetch("https://www.googleapis.com/calendar/v3/calendars/primary/events?" + new URLSearchParams({
+    //     singleEvents: true,
+    //     orderBy: 'startTime',
+    //     maxResults: 10,
+    //     timeMin: '2023-04-10T23:59:59-08:00'
+    //   }), {
+    //     method: "GET",
+    //     headers: {
+    //       'Authorization': 'Bearer ' + session.provider_token
+    //     },
+    //   }).then((res) => {
+    //     return res.json()
+    //   }).then((response) => {
+    //     setUpcomingEvents(response.items)
+    //     setUpcomingEventNames(response.items.map(({ summary }) => summary))
+    //   })
+    // }
   
     return (
-    <div>
-        <p>Start of event</p>
+    <div className={styles.container}>
+        <label>Start of event</label>
         <LocalizationProvider dateAdapter={AdapterDayjs}>
             <DateTimePicker onChange={setStart} value={start}/>
             </LocalizationProvider>
-        <p>End of event</p>
+        <label>End of event</label>
         <LocalizationProvider dateAdapter={AdapterDayjs}>
             <DateTimePicker onChange={setEnd} value={end}/>
             </LocalizationProvider>
-        <p>Event name</p>
-        <input type="text" onChange={(e) => setEventName(e.target.value)} />
-        <p>Event description</p>
+        <label>Event name</label>
+        <input type="text" onChange={(e) => setEventName(e.target.value)}/>
+        <label>Event description</label>
         <input type="text" onChange={(e) => setEventDescription(e.target.value)} />
         <button onClick={() => createCalendarEvent()}>Create Calendar Event</button>
-        <button onClick={() => getCalendarEvent()}>Get Event</button>
+        {/* <button onClick={() => getCalendarEvent()}>Get Event</button>
         { upcomingEvents.length > 0 ? 
         <div>
         <div> Upcoming events: </div> 
         <div> {upcomingEventNames.join('\r')} </div>
         </div>:
-        <div></div>}
+        <div></div>} */}
     </div>
     );
 }
--- client/src/Tasks.js
+++ client/src/Tasks.js
@@ -1,4 +1,5 @@
 import { useState, useEffect, useRef } from 'react'
+import Calendar from './Calendar'
 import styles from './Tasks.module.scss'
 
 
@@ -10,8 +11,8 @@ function CalendarPopup({ task, onClose, buttonRef }) {
       if (buttonRef.current && popupRef.current) {
         const buttonRect = buttonRef.current.getBoundingClientRect();
         const popupRect = popupRef.current.getBoundingClientRect();
-        const topPosition = buttonRect.top - popupRect.height - 150;
-        const leftPosition = buttonRect.left + (buttonRect.width) - (popupRect.width / 2) - 125;
+        const topPosition = buttonRect.top - popupRect.height + 50;
+        const leftPosition = buttonRect.left + (buttonRect.width / 2) - popupRect.width;
         
         popupRef.current.style.top = `${topPosition}px`;
         popupRef.current.style.left = `${leftPosition}px`;
@@ -24,8 +25,8 @@ function CalendarPopup({ task, onClose, buttonRef }) {
 
   return (
     <div className={styles.calendarPopup} ref={popupRef}>
-      <h2>Add to Calendar: {task.text}</h2>
-      {/* You can add your content here */}
+      <h2>Add to Calendar: {task.name}</h2>
+      <Calendar />
       <button onClick={onClose}>Close</button>
     </div>
   );
@@ -40,7 +41,7 @@ export default function Tasks() {
 
   const addTask = () => {
     if (inputValue.trim()) {
-      setTasks([...tasks, { id: Date.now(), text: inputValue, completed: false }]);
+      setTasks([...tasks, { id: Date.now(), name: inputValue, completed: false }]);
       setInputValue('');
     }
   };
@@ -55,45 +56,48 @@ export default function Tasks() {
 
   return (
     <div className={styles.main}>
-      <h3>To-Do List</h3>
-      <div>
-      <form onSubmit={(e) => {
-        e.preventDefault()
-        addTask()
-      }}>
-        <input
-          type="text"
-          value={inputValue}
-          onChange={(e) => setInputValue(e.target.value)}
-          placeholder="Enter a task"
-        />
-        <button type="submit">Add Task</button>
-      </form>
+      <div className={styles.container}>
+        <h3>To-Do List</h3>
+        <div>
+        <form onSubmit={(e) => {
+          e.preventDefault()
+          addTask()
+        }}>
+          <input
+            type="text"
+            value={inputValue}
+            maxLength="128"
+            onChange={(e) => setInputValue(e.target.value)}
+            placeholder="Enter a task"
+          />
+          <button type="submit">Add Task</button>
+        </form>
+        </div>
+        <ul>
+          {tasks.map((task) => (
+            <li key={task.id}>
+              <div className={styles.task}>
+                <input
+                  type="checkbox"
+                  id={`task-${task.id}`}
+                  checked={task.completed}
+                  onChange={() => toggleTask(task.id)}
+                />
+                <label htmlFor={`task-${task.id}`} style={{ textDecoration: task.completed ? 'line-through' : 'none' }}>{task.name}</label>
+              </div>
+              <div className={styles.buttons}>
+                <button onClick={() => deleteTask(task.id)}>Delete</button>
+                <button ref={buttonRef} onClick={() => setActivePopup(task.id)}>Add to Calendar</button>
+              </div>
+              <div style={{position: 'absolute'}}>
+              {activePopup === task.id && (
+                <CalendarPopup task={task} onClose={() => setActivePopup(null)} buttonRef={buttonRef}/>
+              )}
+              </div>
+            </li>
+          ))}
+        </ul>
       </div>
-      <ul>
-        {tasks.map((task) => (
-          <li key={task.id}>
-            <div className={styles.task}>
-              <input
-                type="checkbox"
-                id={`task-${task.id}`}
-                checked={task.completed}
-                onChange={() => toggleTask(task.id)}
-              />
-              <label htmlFor={`task-${task.id}`} style={{ textDecoration: task.completed ? 'line-through' : 'none' }}>{task.text}</label>
-            </div>
-            <div className={styles.buttons}>
-              <button onClick={() => deleteTask(task.id)}>Delete</button>
-              <button ref={buttonRef} onClick={() => setActivePopup(task.id)}>Add to Calendar</button>
-            </div>
-            <div style={{position: 'absolute'}}>
-            {activePopup === task.id && (
-              <CalendarPopup task={task} onClose={() => setActivePopup(null)} buttonRef={buttonRef}/>
-            )}
-            </div>
-          </li>
-        ))}
-      </ul>
     </div>
   );
 }
