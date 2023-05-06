20a8893ccb4aa9967f025cab658db5b4321813e5
Andy Ren <andyren33@gmail.com>
2023-05-05 19:35:41 -0700

    Connect task list to Supabase table

--- src/Tasks.js
+++ src/Tasks.js
@@ -35,34 +35,70 @@ function CalendarPopup({ task, onClose, buttonRef }) {
 
 
 export default function Tasks() {
-  const { supabase } = GoogleLogin()
+  const { supabase, user } = GoogleLogin();
   const [tasks, setTasks] = useState([]);
   const [inputValue, setInputValue] = useState('');
   const buttonRef = useRef(null);
 
-  async function addTask() {
+  const addTask = async() => {
     if (inputValue.trim()) {
-      const { error } = await supabase
+      const { data, error } = await supabase
       .from('todos')
       .insert({
-        tasks: `${inputValue}`
+        task: `${inputValue}`,
+        email: `${user.email}`,
       })
+      .select()
 
       if (error) {
         console.log(error);
       }
-      setTasks([...tasks, { id: Date.now(), name: inputValue, completed: false }]);
-      setInputValue('');
+
+      if (data) {
+        getTasks();
+      }
     }
   };
+
+  const getTasks = async() => {
+    const { data, error } = await supabase
+    .from('todos')
+    .select('*')
+    .eq('email', user.email)
+
+    if (data) {
+      setTasks(data)
+      setInputValue("")
+    }
+
+    if (error) {
+      console.log(error)
+    }
+  }
 
   const toggleTask = (id) => {
     setTasks(tasks.map((task) => task.id === id ? { ...task, completed: !task.completed } : task));
   };
 
-  const deleteTask = (id) => {
-    setTasks(tasks.filter((task) => task.id !== id));
+  const deleteTask = async(id) => {
+    const { error } = await supabase
+    .from('todos')
+    .delete()
+    .eq('id', id)
+
+    if(error) {
+      console.log(error);
+    }
+
+    getTasks();
   };
+
+  useEffect(() => {
+    if (user) {
+      getTasks();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [user])
 
   return (
     <div className={styles.main}>
@@ -93,7 +129,7 @@ export default function Tasks() {
                   checked={task.completed}
                   onChange={() => toggleTask(task.id)}
                 />
-                <label htmlFor={`task-${task.id}`} style={{ textDecoration: task.completed ? 'line-through' : 'none' }}>{task.name}</label>
+                <label htmlFor={`task-${task.id}`} style={{ textDecoration: task.completed ? 'line-through' : 'none' }}>{task.task}</label>
               </div>
               <div className={styles.buttons}>
                 <button onClick={() => deleteTask(task.id)}>Delete</button>
@@ -101,11 +137,6 @@ export default function Tasks() {
                   <CalendarPopup task={task} buttonRef={buttonRef} closeOnDocumentClick/>
                 </Popup>
               </div>
-              {/* <div style={{position: 'absolute'}}>
-              {activePopup === task.id && (
-                <CalendarPopup task={task} onClose={() => setActivePopup(null)} buttonRef={buttonRef}/>
-              )}
-              </div> */}
             </li>
           ))}
         </ul>
